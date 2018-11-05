10454dada7da212b89ef4221025d2a875d5f3f2e
Gabriela Suriano <surianogabriela@gmail.com>
2018-11-05 21:33:24 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'; /* code change */
+import { createStore } from 'redux';
 import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
@@ -12,4 +12,4 @@ const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ); /* code change */
 
-ReactDOM.render(<App store={store} />, document.getElementById('root')); 
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
